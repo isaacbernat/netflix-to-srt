bc638e8e6021da78bed903117804fd6c1bd066c6
isaacbernat <bernat@wrapp.com>
2015-10-18 21:50:57 +0200

    Move to_srt logic into a function

--- to_srt.py
+++ to_srt.py
@@ -20,42 +20,45 @@ def convert_time(raw_time):
     return "{}:{}:{},{}".format(hour, minute, second, ms)
 
 
-text = u""
-with codecs.open(FILE_NAME, 'rb', "utf-8") as f:
-    text = f.read()
-
-sub_lines = (l for l in text.split("\n") if l.startswith("<p begin="))
-subs = []
-prev_time = {"start": 0, "end": 0}
-prev_content = []
-for s in sub_lines:
-    start = re.search(u'begin\="([0-9]*)', s).group(1)
-    end = re.search(u'end\="([0-9]*)', s).group(1)
-    content = re.search(u'xml\:id\=\"subtitle[0-9]+\">(.*)</p>', s).group(1)
-    alt_content = re.search(u'<span style=\"style_0\">(.*)</span>', s)
-    if alt_content:  # some background text has additional styling
-        content = alt_content.group(1)
-    prev_start = prev_time["start"]
-    if (prev_start == start and prev_time["end"] == end) or not prev_start:
-        # there may be multiple lines starting at the same time. This fixes it.
+def to_srt(text):
+    sub_lines = (l for l in text.split("\n") if l.startswith("<p begin="))
+    subs = []
+    prev_time = {"start": 0, "end": 0}
+    prev_content = []
+    for s in sub_lines:
+        start = re.search(u'begin\="([0-9]*)', s).group(1)
+        end = re.search(u'end\="([0-9]*)', s).group(1)
+        content = re.search(u'xml\:id\=\"subtitle[0-9]+\">(.*)</p>', s).group(1)
+        alt_content = re.search(u'<span style=\"style_0\">(.*)</span>', s)
+        if alt_content:  # some background text has additional styling
+            content = alt_content.group(1)
+        prev_start = prev_time["start"]
+        if (prev_start == start and prev_time["end"] == end) or not prev_start:
+            # Fix for multiple lines starting at the same time
+            prev_time = {"start": start, "end": end}
+            prev_content.append(content)
+            continue
+        subs.append({
+            "start_time": convert_time(prev_time["start"]),
+            "end_time": convert_time(prev_time["end"]),
+            "content": u"\n".join(prev_content),
+            })
         prev_time = {"start": start, "end": end}
-        prev_content.append(content)
-        continue
+        prev_content = [content]
     subs.append({
-        "start_time": convert_time(prev_time["start"]),
-        "end_time": convert_time(prev_time["end"]),
+        "start_time": convert_time(start),
+        "end_time": convert_time(end),
         "content": u"\n".join(prev_content),
-        })
-    prev_time = {"start": start, "end": end}
-    prev_content = [content]
-subs.append({
-    "start_time": convert_time(start),
-    "end_time": convert_time(end),
-    "content": u"\n".join(prev_content),
-})
+    })
 
-with codecs.open(FILE_NAME + ".srt", 'wb', "utf-8") as f:
     lines = (u"{}\n{} --> {}\n{}\n".format(
         s+1, subs[s]["start_time"], subs[s]["end_time"], subs[s]["content"])
         for s in range(len(subs)))
-    f.write(u"\n".join(lines))
+    return u"\n".join(lines)
+
+
+with codecs.open(FILE_NAME, 'rb', "utf-8") as f:
+    text = f.read()
+
+with codecs.open(FILE_NAME + ".srt", 'wb', "utf-8") as f:
+    f.write(to_srt(text))
