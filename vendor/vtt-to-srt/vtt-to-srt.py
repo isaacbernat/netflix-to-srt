9fb202fe104029f02ee96c4cd4cc4ad9dbe74dbc
lbrayner <lbrayner@users.noreply.github.com>
2019-06-10 22:55:59 -0300

    ensures pip plus subprocess

--- vendor/vtt-to-srt/vtt-to-srt.py
+++ vendor/vtt-to-srt/vtt-to-srt.py
@@ -5,30 +5,29 @@ __version__ = "2.1"
 __maintainer__ = "Leonardo Brayner e Silva"
 __email__ = "brayner.l@gmail.com"
 
-# https://thesmithfam.org/blog/2012/10/25/temporarily-suppress-console-output-in-python
-# from contextlib import contextmanager
-# import sys
-# import os
-# import tempfile
-
-# @contextmanager
-# def suppress_stdout():
-#     _, temp_path = tempfile.mkstemp()
-#     stdout_redir = open(temp_path, "w")
-#     old_stdout = sys.stdout
-#     sys.stdout = stdout_redir
-#     try:  
-#         yield
-#     finally:
-#         sys.stdout = old_stdout
-
-# https://stackoverflow.com/a/37961211
-import contextlib
 import tempfile
-import pip
+import subprocess
 import sys
 from functools import reduce
 
+_, temp_path = tempfile.mkstemp()
+temp_redir = open(temp_path, "w+")
+
+def ensure_pip():
+    subprocess.call([sys.executable, '-m', 'ensurepip', '--default-pip'],
+        stdout=temp_redir,stderr=temp_redir)
+
+ensure_pip()
+
+def install(directive):
+    command = [sys.executable, '-m', 'pip', 'install']
+    if type(directive) is list:
+        command = command + directive
+    else:
+        command.append(directive)
+    subprocess.call(command,stdout=temp_redir,stderr=temp_redir)
+
+# https://stackoverflow.com/a/37961211
 def import_or_install(package):
     if type(package) is tuple:
         assembly = package[0]
@@ -40,37 +39,19 @@ def import_or_install(package):
         __import__(assembly)
         return False
     except ImportError:
-        pip.main(['install', name])       
+        install(name)
         return True
 
 def import_packages():
-    _, temp_path = tempfile.mkstemp()
-    temp_redir = open(temp_path, "w")
-    with contextlib.redirect_stderr(temp_redir):
-        with contextlib.redirect_stdout(temp_redir):
-            return reduce((lambda x, y: x or y),
-                    [import_or_install("wheel"), 
-                        import_or_install(("webvtt","webvtt-py")),
-                        import_or_install("html2text"),
-                        import_or_install("pysrt")])
-            # return (import_or_install("wheel")
-            #         or import_or_install(("webvtt","webvtt-py"))
-            #         or import_or_install("html2text")
-            #         or import_or_install("pysrt"))
+    install("wheel") # sidestepping a bug
+    return reduce((lambda x, y: x or y),
+            [import_or_install(("webvtt","webvtt-py")),
+                import_or_install("html2text"),
+                import_or_install("pysrt")])
 
 if import_packages():
-    print("Ran for the first time and imported packages. Please run again.")
+    print("Ran for the first time and installed packages. Please try again.")
     sys.exit(0)
-
-# import subprocess
-
-# # https://stackoverflow.com/a/50255019
-# def install(package):
-#     subprocess.call([sys.executable, "-m", "pip", "install", package])
-
-# install("html2text")
-# install("pysrt")
-# install("webvtt-py")
 
 import os
 from webvtt import WebVTT
